39ddb4f24e742677245cbd0a7edda3494366550a
gui2one <guillaume.rouault.fx@gmail.com>
2025-02-24 16:50:41 +0100

    styling

--- odivino-menu-de-la-semaine-template.php
+++ odivino-menu-de-la-semaine-template.php
@@ -31,19 +31,32 @@ function get_plats_category($category_slug1, $category_slug2) {
    return $posts;
 }
 
+
+function display_items($posts, $title = "title") {
+
+   echo "<div class=\"odivino-plat-category-container\">";
+   echo "<h2 class=\"italian-title\">$title</h2>";
+   foreach ($posts as $post) {
+       echo "<div class=\"odivino-plat-container\">";
+       echo "<h4>$post->post_title</h4>";
+       echo $post->post_content;
+       echo "</div>";
+   }
+   echo "</div>";
+}
+
 $entrees = get_plats_category("menu-de-la-semaine", "entrees");
+$plats = get_plats_category("menu-de-la-semaine", "plats");
+$desserts = get_plats_category("menu-de-la-semaine", "desserts");
 ?>
 <main class="odivino">
  <div style="height:150px;"></div>
 
    <h1>Menu de la Semaine</h1>
    <?php
-      foreach ($entrees as $post) {
-         echo "<div class=\"odivino-plat-container\">";
-         echo "<h4>$post->post_title</h4>";
-         echo $post->post_content;
-         echo "</div>";
-      }
+      display_items($entrees, "Entrées");
+      display_items($plats, "Plats");
+      display_items($desserts, "Desserts");
 
    ?>
 
