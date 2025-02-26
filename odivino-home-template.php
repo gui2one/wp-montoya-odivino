8cd97c228e320c2450512213c6e9d2ebbeb4341d
gui2one <guillaume.rouault.fx@gmail.com>
2025-02-26 13:08:32 +0100

    home template

---
+++ odivino-home-template.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Odivino Home Page Template
+ * Description: A custom page template for the Odivino page.
+ * Author: gui2one
+ */
+
+get_header(); ?>
+
+<main class="odivino">
+ <div style="height:150px;"></div>
+
+    <?php the_content(); ?>
+ </main>
+ <?php get_footer(); ?>
