af0c49aebeab645b90365d83a57c016268c95e33
gui2one <guillaume.rouault.fx@gmail.com>
2025-02-27 15:41:53 +0100

    page a emporter template

---
+++ odivno-a-empoter-template.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Template Name: Odivino A Emporter Page Template
+ * Description: A custom page template for the Odivino page.
+ * Author: gui2one
+ */
+
+ get_header(); ?>
+<?php
+function get_plats_category($category_slug1, $category_slug2) {
+    $posts = get_posts( array(
+        'post_type' => 'plats',
+        'posts_per_page' => -1,
+        'tax_query' => array(
+            'relation' => 'AND',
+            array(
+                'taxonomy' => 'plats-category',
+                'field' => 'slug',
+                'terms' => $category_slug1,
+            ),
+            array(
+                'taxonomy' => 'plats-category',
+                'field' => 'slug',
+                'terms' => $category_slug2,
+            ),
+        ),
+    ) );
+ 
+    return $posts;
+ }
+
+function get_pizzas_category($category_slug) {
+    $posts = get_posts( array(
+        'post_type' => 'pizzas',
+        'posts_per_page' => -1,
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'pizzas-category',
+                'field' => 'slug',
+                'terms' => $category_slug,
+            ),
+        ),
+    ) );
+
+    return $posts;
+}
+function get_base_tomate() {
+    $posts = get_pizzas_category("base-tomate");
+
+    return $posts;
+}
+
+function get_base_mozzarella() {
+    $posts = get_pizzas_category("base-mozzarella-fior-di-latte");
+
+    return $posts;
+}
+
+
+function get_speciales() {
+    $posts = get_pizzas_category("speciales");
+
+    return $posts;
+}
+
+function display_items($posts, $title = "title") {
+
+    echo "<div class=\"odivino-plat-category-container\">";
+    echo "<h4 class=\"italian-title\">$title</h4>";
+    foreach ($posts as $post) {
+        echo "<div class=\"odivino-plat-container\">";
+        echo "<h5>$post->post_title</h5>";
+        echo $post->post_content;
+        echo "</div>";
+    }
+    echo "</div>";
+}
+?>
+
+ <main class="odivino">
+  <div style="height:150px;"></div>
+    <h1>A Emporter</h1>
+    <h3>Les Pizzas</h3>
+    <?php 
+
+        display_items(get_base_tomate(), "Base Tomate");
+        display_items(get_base_mozzarella(), "Base Mozzarella");
+        display_items(get_speciales(), "Les Spéciales");
+    ?>
+    <h3>Gourmandises</h3>
+    <?php
+    
+        $plats_emporter = get_plats_category("desserts", "a-emporter");
+        display_items($plats_emporter, "Desserts");
+    ?>
+  </main>
+  <?php get_footer(); ?>
