a61ffddb9f406afc6ccdfcc0d90e3c743f263e5f
gui2one <guillaume.rouault.fx@gmail.com>
2025-06-19 11:18:44 +0200

    titles

--- templates/odivino-home-template.php
+++ templates/odivino-home-template.php
@@ -1,29 +1,30 @@
 <?php
-    /**
-     * Template Name: Odivino Home Page Template
-     * Description: A custom page template for the Odivino Home page.
-     * Author: gui2one
-     */
+
+/**
+ * Template Name: Odivino Home Page Template
+ * Description: A custom page template for the Odivino Home page.
+ * Author: gui2one
+ */
 
 get_header(); ?>
 
 <div id="odivino_home_header">
   <div class="image-container">
     <?php
-        $image = get_the_post_thumbnail_url(get_the_ID(), "full");
-        if ($image) {
-            echo "<img src=" . esc_url($image) . ">";
-        }
+    $image = get_the_post_thumbnail_url(get_the_ID(), "full");
+    if ($image) {
+      echo "<img src=" . esc_url($image) . ">";
+    }
     ?>
   </div> <!-- END image-container -->
   <div class="content">
-    <h1 class="odivino-animated-title">O'Divino</h1>
+    <h1 class="odivino-animated-title">O'Divino - Restaurant Italien à Melesse</h1>
 
     <span class="odivino-sub-title"><?php echo get_option("blogdescription"); ?></span>
   </div>
-    </div> <!-- END header -->
+</div> <!-- END header -->
 <main class="odivino">
- <!-- <div style="height:150px;"></div> -->
-    <?php the_content(); ?>
- </main>
- <?php get_footer(); ?>
+  <!-- <div style="height:150px;"></div> -->
+  <?php the_content(); ?>
+</main>
+<?php get_footer(); ?>
--- todos.md
+++ todos.md
@@ -2,3 +2,27 @@
 
 - pizzas dans la carte
 - selection addresse et tel
+
+## referencement
+
+- titre : ajouter le mot clé et la localisation
+- meta description plus courte et plus parlante
+
+- entre 3 et 5 mots clés secondaires a metter dans le texte. 400 minimum.
+
+- retirer la balise strong dans les titres H1
+
+### annuaires
+
+- inscription a des annuaires.
+  (https://iansorin.fr/chercher-spot-ninjalinking/)
+  1- https://hop-plats.fr/annuaire-restaurants-selection.php?ville=SAINT%20MALO&cat=22
+
+### reseaux sociaux
+
+- mettre un lien en fin de poste a chaque fois.
+
+### La CARTE / A EMPORTER / Menu DE semaine
+
+- meta descrription !!!
+- titre - mot clé secondaire.
